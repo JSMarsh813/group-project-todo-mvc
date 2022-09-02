fcc959b73977ef5c2aab062b8f36a60afa24140e
Justin <juc81@pitt.edu>
2022-09-01 23:30:35 -0400

    added Pokemon schema to structure names of poke, added basic fuzzy search for mongo search index

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express') //loading express
 const app = express() //can call express easily
 const mongoose = require('mongoose')
@@ -42,7 +41,33 @@ app.use(flash())
   
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
- 
+
+//Placeholder in our server file for the search index to read names in our pokemon collection 
+app.get("#", async (req,res) => {
+  try {
+      let result = await collection.aggregate([ //wait for our result to aggregate data together
+          {
+              "$search" : { //tell mongo to search
+                  "autocomplete" : { //type of search
+                      "query": `${req.query.query}`, //our search query
+                      "path": "name", 
+                      "fuzzy": { //search type
+                          "maxEdits": 2, //our user can make 2 spelling errors/characters when searching for a poke and we'll substitute missing or wrong characters
+                          "prefixLength": 3 //user needs to type atleast 3 characters
+                      }
+                  }
+              }
+          }
+      ]).toArray() //dump the return into array so our JS knows what to do with it
+      res.send(result)
+      // console.log(result)
+  } catch (error) {
+      res.status(500).send({message: error.message})
+      // console.log(error)
+  }
+})
+
+
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
